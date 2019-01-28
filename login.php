49202ea116355d34b5f1a3c1fec39ba2b50b60c0
root <root@ip-172-31-45-140.us-east-2.compute.internal>
2019-01-28 11:28:03 -0600

    Need to finish editing upload.php

--- login.php
+++ login.php
@@ -1,4 +1,5 @@
 <?php
+ session_start();
  if($_POST['Submit']){
   $file = fopen("/srv/uploads/users.txt", 'a') or die("Unable to open file!");
   $user = $_POST['user'];
@@ -18,7 +19,8 @@
   if(!$exists){
    echo "User does not exist!";
   }else{
-   header('Location:http://ec2-52-15-187-19.us-east-2.compute.amazonaws.com/~elijahpena/spring2019-module2-group-455281-467000/upload.html?user='.$user);
+   $_SESSION["user"] = $user;
+   header('Location:http://ec2-52-15-187-19.us-east-2.compute.amazonaws.com/~elijahpena/spring2019-module2-group-455281-467000/upload.html');
 
   }
   fclose($file);
--- upload.php
+++ upload.php
@@ -1,14 +1,28 @@
 <?php
+session_start();
+$user = $_SESSION["user"];
+$dir = sprintf("/srv/uploads/%s", $user);
 $filename = basename($_FILES['fileToUpload']['name']);
 if( !preg_match('/^[\w_\.\-]+$/', $filename) ){
 	echo "Invalid filename";
 	exit;
 }
-$full_path = sprintf("/srv/uploads/%s", $filename);
+$full_path = sprintf("/srv/uploads/%s/%s", $user, $filename);
 if( move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $full_path) ){
 	echo "worked";
 }else{
 	echo "failed";
 }
+
+  3  $dir = "/var/www/users/{$user}";
+  4  if (is_dir($dir)) {
+  5   if ($dh = opendir($dir)) {
+  6    while (($file = readdir($dh)) !== false) {
+  7     echo nl2br("filename: {$file}  . \n");
+  8    }
+  9    closedir($dh);
+ 10   }
+ 11  }
+
 
 ?>
