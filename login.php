43ecacc42e6a3c58a75f24eeb4721f0bc670fc15
root <root@ip-172-31-45-140.us-east-2.compute.internal>
2019-01-28 11:01:45 -0600

    Proper redirecting

--- login.php
+++ login.php
@@ -1,8 +1,8 @@
 <?php
  if($_POST['Submit']){
-  $file = fopen("/var/www/users/user.txt", 'a+') or die("Unable to open file!");
+  $file = fopen("/srv/uploads/users.txt", 'a') or die("Unable to open file!");
   $user = $_POST['user'];
-  $lines = file("/var/www/users/user.txt");
+  $lines = file("/srv/uploads/users.txt");
   $exists = 0;
   foreach ($lines as $line_num => $line) {
    if($line == $user."\n"){
@@ -16,16 +16,12 @@
    //echo $line . "<br/>\n";
   }
   if(!$exists){
-   fwrite($file, $user."\n");
-   $filePath = "/var/www/users/{$user}";
-   echo nl2br("user created\n");
-   mkdir($filePath);
+   echo "User does not exist!";
+  }else{
+   header('Location:http://ec2-52-15-187-19.us-east-2.compute.amazonaws.com/~elijahpena/spring2019-module2-group-455281-467000/upload.html?user='.$user);
+
   }
   fclose($file);
-  $filePath = $filePath . '/hello.txt';
-  //$testing = fopen($filePath, 'a+') or die("Unable to open file!");
-  //fclose($file);
-  header('Location:http://ec2-52-15-187-19.us-east-2.compute.amazonaws.com/~elijahpena/spring2019-module2-group-455281-467000/files.php?user='.$user);
   //echo "worked";
 
 
--- upload.php
+++ upload.php
@@ -1,15 +1,14 @@
 <?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
- if(move_uploaded_file($target_file, $target_dir)){
-   echo "worked"  
- } else {
-  echo "didnt work"
- }
-
+$filename = basename($_FILES['fileToUpload']['name']);
+if( !preg_match('/^[\w_\.\-]+$/', $filename) ){
+	echo "Invalid filename";
+	exit;
 }
+$full_path = sprintf("/srv/uploads/%s", $filename);
+if( move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $full_path) ){
+	echo "worked";
+}else{
+	echo "failed";
+}
+
 ?>
