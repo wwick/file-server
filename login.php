78197da031149564a307294cdf300fb6daff1e8e
wwick <wwick@wustl.edu>
2019-01-28 21:59:49 -0600

    monday night 2

--- login.php
+++ login.php
@@ -1,10 +1,10 @@
 <?php
 
-$URL = "http://ec2-52-15-187-19.us-east-2.compute.amazonaws.com/~elijahpena/spring2019-module2-group-455281-467000/upload.html";
+//$URL = "http://ec2-52-15-187-19.us-east-2.compute.amazonaws.com/~elijahpena/spring2019-module2-group-455281-467000/upload.html";
 $URL = "http://ec2-52-14-191-6.us-east-2.compute.amazonaws.com/~wwick/git/spring2019-module2-group-455281-467000/upload.html";
 
 session_start();
-if(isset($_POST['submit'])) {
+if(isset($_POST['Submit'])) {
     //opens users.txt, which contains the list of usernames
     $file = fopen("/srv/uploads/users.txt", 'a') or die("Unable to open file!");
     $user = $_POST['user'];
@@ -20,7 +20,7 @@ if(isset($_POST['submit'])) {
     }
     //posts error message and exits if user is not in the list of usernames
     if(!$exists) {
-        die("User does not exist");
+	die("User does not exist");
     }
     //sends users to their list of files
     $_SESSION["user"] = $user;
