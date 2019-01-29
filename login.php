fe01838e24bc141660830406ccfcc75d2c8fe0f3
wwick <wwick@wustl.edu>
2019-01-28 21:08:22 -0600

    monday night 1

--- login.php
+++ login.php
@@ -1,31 +1,30 @@
 <?php
- session_start();
- if($_POST['Submit']){
-  $file = fopen("/srv/uploads/users.txt", 'a') or die("Unable to open file!");
-  $user = $_POST['user'];
-  $lines = file("/srv/uploads/users.txt");
-  $exists = 0;
-  foreach ($lines as $line_num => $line) {
-   if($line == $user."\n"){
-    echo nl2br("user exists\n");
-    $exists = 1;
-    break;
-   } else {
-    //fwrite($file, $user);
-    //echo "user created";
-   }
-   //echo $line . "<br/>\n";
-  }
-  if(!$exists){
-   echo "User does not exist!";
-  }else{
-   $_SESSION["user"] = $user;
-   header('Location:http://ec2-52-15-187-19.us-east-2.compute.amazonaws.com/~elijahpena/spring2019-module2-group-455281-467000/upload.html');
 
-  }
-  fclose($file);
-  //echo "worked";
+$URL = "http://ec2-52-15-187-19.us-east-2.compute.amazonaws.com/~elijahpena/spring2019-module2-group-455281-467000/upload.html";
+$URL = "http://ec2-52-14-191-6.us-east-2.compute.amazonaws.com/~wwick/git/spring2019-module2-group-455281-467000/upload.html";
 
+session_start();
+if(isset($_POST['submit'])) {
+    //opens users.txt, which contains the list of usernames
+    $file = fopen("/srv/uploads/users.txt", 'a') or die("Unable to open file!");
+    $user = $_POST['user'];
+    $lines = file("/srv/uploads/users.txt");
+    fclose($file);
+    $exists = 0;
+    //checks that the user is in the list of usernames
+    foreach ($lines as $line_num => $line) {
+        if($line == $user."\n"){
+            $exists = 1;
+            break;
+        }
+    }
+    //posts error message and exits if user is not in the list of usernames
+    if(!$exists) {
+        die("User does not exist");
+    }
+    //sends users to their list of files
+    $_SESSION["user"] = $user;
+    header("Location:".$URL);
+}
 
- }
 ?>
--- upload.php
+++ upload.php
@@ -1,28 +1,27 @@
 <?php
 session_start();
 $user = $_SESSION["user"];
+
 $dir = sprintf("/srv/uploads/%s", $user);
 $filename = basename($_FILES['fileToUpload']['name']);
-if( !preg_match('/^[\w_\.\-]+$/', $filename) ){
-	echo "Invalid filename";
-	exit;
+if(!preg_match('/^[\w_\.\-]+$/', $filename)){
+  die("invalid filename");
 }
 $full_path = sprintf("/srv/uploads/%s/%s", $user, $filename);
-if( move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $full_path) ){
+if(move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $full_path)){
 	echo "worked";
-}else{
+} else {
 	echo "failed";
 }
 
-  3  $dir = "/var/www/users/{$user}";
-  4  if (is_dir($dir)) {
-  5   if ($dh = opendir($dir)) {
-  6    while (($file = readdir($dh)) !== false) {
-  7     echo nl2br("filename: {$file}  . \n");
-  8    }
-  9    closedir($dh);
- 10   }
- 11  }
-
+//   $dir = "/var/www/users/{$user}";
+//   if (is_dir($dir)) {
+//   if ($dh = opendir($dir)) {
+//   while (($file = readdir($dh)) !== false) {
+//   echo nl2br("filename: {$file}  . \n");
+//   8    }
+//   9    closedir($dh);
+//  10   }
+//  11  }
 
 ?>
