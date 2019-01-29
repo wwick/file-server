d7accebb778092c95684d6028c0b2fb25310d1a2
wwick <wwick@wustl.edu>
2019-01-29 00:07:17 -0600

    start of styling

--- files.php
+++ files.php
@@ -4,6 +4,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>File Server</title>
+    <style type=text/css>
+    table {
+        font: 12px/16px Verdana, sans-seriff;
+    }
+    </style>
 </head>
 <body>
 
@@ -22,9 +27,8 @@
             while (($file = readdir($dh)) !== false) {
                 echo "<tr>";
                 echo "<th>$file</th>";
-                echo "<th><input type =\"button\">Download</input></th>";
-                echo "<th><input type =\"button\">Delete</input></th>";
-                // echo nl2br("filename: {$file}  . \n");
+                echo "<th><input type =\"button\" value=\"Download\"/></th>";
+                echo "<th><input type =\"button\" value=\"Delete\"/></th>";
                 echo "</tr>";
             }
         closedir($dh);
@@ -32,8 +36,9 @@
     }
     ?>
 </table>
-
-<form action="upload.php" method="post" enctype="multipart/form-data">
+<br>
+<!-- form to upload file  -->
+<form action="upload.php" method="POST" enctype="multipart/form-data">
     Select a file to upload:
     <input type="file" name="fileToUpload">
     <input type="submit" value="Upload" name="submit">
