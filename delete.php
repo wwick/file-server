55184c6528baa919fea08bb646d53810f3ae3315
wwick <wwick@wustl.edu>
2019-01-30 11:18:32 -0600

    trying to fix delete button

--- delete.php
+++ delete.php
@@ -7,4 +7,5 @@
     $dir = "/srv/uploads/users/".$user;
     $fileName = basename($_SESSION["file"]);
     unlink($dir."/".$fileName);
+    header("Location:http://ec2-52-14-191-6.us-east-2.compute.amazonaws.com/~wwick/git/spring2019-module2-group-455281-467000/login.html");
 ?>
