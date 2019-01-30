faac00db8965731c38217dc42f44d6491ed42df1
root <root@ip-172-31-45-140.us-east-2.compute.internal>
2019-01-30 10:50:22 -0600

    please work

--- abort.php
+++ abort.php
@@ -1,4 +1,4 @@
 <?php
-    sessioni_abort();
+    session_abort();
     header("Location: http://ec2-52-14-191-6.us-east-2.compute.amazonaws.com/~wwick/git/spring2019-module2-group-455281-467000/login.html");
 ?>
