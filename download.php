aed1544e2a0be668ec7dfe8368a09d944fa747ce
wwick <wwick@wustl.edu>
2019-02-01 17:35:31 -0600

    start of download.php

---
+++ download.php
@@ -0,0 +1,11 @@
+<?php
+    session_start();
+    if(!isset(_SESSION["user"])){
+        header("Location:login.html");
+    }
+    $user = $_SESSION["user"];
+    $file = $_REQUEST["file"];
+    $filepath = "/srv/uploads/users/".$user."/".$file;
+    readfile($filepath);
+    header("Location:files.php");
+?>
