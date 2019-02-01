51d3336eb995a5779e21e7de9649a9631abec959
Elijah Pena <elijahpena@ip-172-31-45-140.us-east-2.compute.internal>
2019-02-01 17:34:39 -0600

    no longer url dependent

--- files.php
+++ files.php
@@ -44,7 +44,7 @@ tbody {
     <?php
     session_start();
     if (!isset($_SESSION["user"])) {
-        header("Location:http://ec2-52-14-191-6.us-east-2.compute.amazonaws.com/~wwick/git/spring2019-module2-group-455281-467000/login.html");
+        header("Location:login.html");
     }
     $user = $_SESSION["user"];
     $dir = "/srv/uploads/users/{$user}";
