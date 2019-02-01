7850d0a12e27107e95c7039d3c17392129fee0b6
Elijah Pena <elijahpena@ip-172-31-45-140.us-east-2.compute.internal>
2019-02-01 17:44:08 -0600

    header info

--- download.php
+++ download.php
@@ -5,6 +5,17 @@
     }
     $user = $_SESSION["user"];
     $file = $_REQUEST["file"];
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename='.basename($file));
+    header('Content-Transfer-Encoding: binary');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($file));
+    ob_clean();
+    flush();
+//the above header information was recieved from https://stackoverflow.com/questions/11315951/using-the-browser-prompt-to-download-a-file
     $filepath = "/srv/uploads/users/".$user."/".$file;
     readfile($filepath);
     header("Location:files.php");
